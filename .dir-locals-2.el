00cfa9c40914b4dd60d63b9e8e2e18347f77e045
Logan C. Brooks <lcbrooks@andrew.cmu.edu>
2022-12-07 03:52:48 -0800

    Add slides WIP

---
+++ .dir-locals-2.el
@@ -0,0 +1,7 @@
+
+((ess-r-mode
+  . ((comment-add . 0) ; to get commenting commands to avoid "##" in order to
+                       ; match this style until
+                       ; https://github.com/emacs-ess/ESS/issues/996 is
+                       ; resolved, but triggering a may-not-be-safe message
+     )))
